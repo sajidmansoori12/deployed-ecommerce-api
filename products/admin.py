932e83e509b808da66b23843e68cfef78a71a44b
M Sajid Mansoori <43899275+sajidmansoori12@users.noreply.github.com>
2020-11-07 09:59:04 +0530

    Add files via upload

---
+++ products/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Product,Banner,Offer
+# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Banner)
+admin.site.register(Offer)
